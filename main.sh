adea1fe2bb27b3723adee2d38d81d4d3a9c32ddb
KCidevsec <122748577+KCidevsec@users.noreply.github.com>
2023-05-31 22:18:13 +0300

    Merged all scripts to main.sh

--- main.sh
+++ main.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 
 #Global Variables
-export root_path=$(pwd)$(echo /)
-export cur_user="$(id -u -n)"
-export os_architecture="$(uname -p)"
-export os_system="$(uname -s)"
+root_path=$(pwd)$(echo /)
+cur_user="$(id -u -n)"
+os_architecture="$(uname -p)"
+os_system="$(uname -s)"
+images=$(sudo docker images | tee >(wc -l) | tail -0)
+number_of_images=$(echo $images | tr -d ' ')
+max_number_of_images="30"
+running_docker_instance=$(sudo docker ps -aq)
 
-#Directories
+#directory
+scripts_directory="scripts/"
 container_dockerfile_directory="container_images/docker_files/"
 container_dockercompose_directory="container-images/docker_compose/"
-scripts_directory="scripts/"
+
 #scripts
 setup_script="setup.sh"
-build_image_script="build_container_images.sh"
-cleanup_script="cleanup.sh"
-labels_script="labels.sh"
+
+#Include Scripts
+source $root_path$scripts_directory$setup_script
+source $root_path$scripts_directory$build_image_script
+source $root_path$scripts_directory$cleanup_script
+source $root_path$scripts_directory$labels_script
+
 #images with Dockerfile
 centos1=$root_path$container_dockerfile_directory"centos1/"
 ftp_anon=$root_path$container_dockerfile_directory"ftp_anon/"
 kali_linux=$root_path$container_dockerfile_directory"kali_linux/"
 proftpd=$root_path$container_dockerfile_directory"proftpd/"
+
 #images with docker compose
 activemq_CVE_2015_5254=$root_path$container_dockercompose_directory"activemq-CVE-2015-5254/"
 activemq_CVE_2016_3088=$root_path$container_dockercompose_directory"activemq-CVE-2016-3088/"
@@ -45,11 +55,9 @@ ssh_CVE_2018_15473=$root_path$container_dockercompose_directory"ssh-CVE-2018-154
 tomcat_CVE_2020_1938=$root_path$container_dockercompose_directory"tomcat-CVE-2020-1938/"
 weblogic_ssrf=$root_path$container_dockercompose_directory"weblogic-ssrf/"
 
-#Include Scripts
-source $root_path$scripts_directory$setup_script
-source $root_path$scripts_directory$build_image_script
-source $root_path$scripts_directory$cleanup_script
-source $root_path$scripts_directory$labels_script
+#lists
+base_images_list=("centos:7" "metabrainz/base-image:latest" "kalilinux/kali-rolling:latest" "ubuntu:14.04")
+dockerfile_list=($centos1 $ftp_anon $kali_linux $proftpd)
 
 start_services(){
     if [[ "$os_system" = "Darwin" ]]
@@ -63,8 +71,70 @@ start_services(){
     fi
 }
 
+label_center()
+{
+    lenght_of_string=$(($(echo $1 | wc -m) - 1))
+    columns=80
+    front_spacing=$(($(($columns - $lenght_of_string)) / 2 ))
+    back_spacing=$(($front_spacing + $lenght_of_string))
+    number_of_front_sympols=5
+    number_of_characters_before_back_sympols=76
+    for i in $(seq 1 $columns); do echo -n "="; done
+    echo ""
+    for i in $(seq 1 $columns); do
+    {
+        if [[ $i -le $number_of_front_sympols ]]
+        then
+            echo -n "="
+        fi
+
+        if [[ $i -lt $front_spacing ]] && [[ $i -gt $number_of_front_sympols ]] 
+        then
+            echo -n " "
+        fi
+
+        if [[ $i -eq $front_spacing ]]
+        then
+            echo -n $1
+        fi
+
+        if [[ $i -gt $back_spacing ]]
+        then
+        {
+            if [[ $i -le $number_of_characters_before_back_sympols ]]
+            then
+                echo -n " "
+            elif [[ $i -gt $number_of_characters_before_back_sympols ]]
+            then
+                echo -n "="
+            fi
+        }
+        fi
+        if [[ $i -eq $columns ]]
+        then
+            echo -n "="
+        fi
+    }
+    done
+    echo ""
+    for i in $(seq 1 $columns); do echo -n "="; done
+    echo ""
+}
+
+check_for_null_variables(){
+    if [ -z "$1" ]; then
+    {
+        echo "Variable $2 is not set - exiting"
+        exit
+    }
+    else
+    {
+        echo "Variable $2 set!"
+    }  
+    fi    
+}
+
 pull_base_images(){
-    base_images_list=("centos:7" "metabrainz/base-image:latest" "kalilinux/kali-rolling:latest" "ubuntu:14.04")
     for base_image_list_item in ${base_images_list[@]}; do
     {
         if [[ -z "$(docker images | awk '{print $1":"$2}' | grep $base_image_list_item)" ]]
@@ -80,21 +150,72 @@ pull_base_images(){
     done
 }
 
+# recieves that path of the docker file and builds an image
+build_image_from_dockerfile()
+{
+    image_name=$(echo "$1" | rev | cut -d/ -f2 | rev)
+    image_exist=false
+    current_images_array=()
+    while IFS= read -r line; do
+        current_images_array+=( "$line" )
+    done < <( sudo docker images | awk '{print $1}' )
+
+    for image_name_item in ${current_images_array[@]}; do
+        if [[ "$image_name_item" == $image_name ]]
+        then
+            $image_exist = true
+        fi
+    done
+
+    if [ $image_exist = false ]
+    then
+        cd $1 && sudo docker buildx build --platform=linux/amd64 -t $image_name .
+    fi
+}
+
 build_images_with_docker_file(){
-    dockerfile_list=($centos1 $ftp_anon $kali_linux $proftpd)
     for dockerfile_list_item in ${dockerfile_list[@]}; do
         build_image_from_dockerfile $dockerfile_list_item
     done
 }
 
-option_1(){
-    echo "Not done yet"
+clear_previous_runs(){
+    echo "fresh(): \$0 is $0"
+    echo "fresh(): \$1 is $1"
+    echo "fresh(): \$2 is $2"
+    echo "fresh(): total args passed to me $#"
+    echo "fresh(): all args (\$@) passed to me -\"$@\""
+    echo "fresh(): all args (\$*) passed to me -\"$*\""
+    #clear previous runs
+    if [ -z "$3" ]
+    then
+    {
+        while IFS= read -r line; do
+            sudo docker stop $line
+            echo "Stoping container with id $line"
+        done <<< "$3"
+    }
+    fi
+    #$number_of_images > $max_number_of_images 
+    if [ "$1" -ge "$2" ]; then
+        echo "Maximum number of image found. Clearing OS"
+        sudo docker rmi $(sudo docker images -q)
+    else
+        echo "Number of allowed images $1/$2"
+    fi
 }
 
-option_2(){
+option_1(){
     label_center "START - PULLING BASE IMAGES"
     pull_base_images
     label_center "END - PULLING BASE IMAGES"
+    label_center "START - BUILDING CONTAINERS"
+    build_images_with_docker_file
+    label_center "END - BUILDING CONTAINERS"
+}
+
+option_2(){
+    lecho "Not done yet"
 }
 
 option_3(){
@@ -106,7 +227,10 @@ option_4(){
 }
 
 option_5(){
-    echo "Not done yet"
+    label_center "START - CLEARING DOCKER ENVIROMENT"
+    echo "The number of images are $number_of_images"
+    clear_previous_runs $number_of_images $max_number_of_images $running_docker_instance
+    label_center "END - CLEARING DOCKER ENVIROMENT"
 }
 
 menu() {
@@ -133,7 +257,7 @@ menu() {
         echo "      Contact: kyriakoskosta@outlook.com"
         echo ""
         echo ""
-        echo "    	1  -  About Security Leaning Hub"
+        echo "    	1  -  Setup Enviroment"
         echo "    	2  -  Network Security Lab"
         echo "    	3  -  Web Application Security Lab"
         echo "    	4  -  Email Security Lab"
